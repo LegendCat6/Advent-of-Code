48686fa102e2abe16f888bf6b916e3cb76890b70
LegendCat6 <6694492+LegendCat6@users.noreply.github.com>
2025-12-02 22:41:14 -0600

    Initial commit

---
+++ day-1.js
@@ -0,0 +1,11 @@
+let left = 'L50';
+left = Number(left.replace(/L/, '-'));
+
+let right = 'R45';
+right = Number(right.replace(/R/, ''));
+
+let answer = left + right
+// answer = answer.replace(/L/, '-');
+
+console.log(answer)
+
